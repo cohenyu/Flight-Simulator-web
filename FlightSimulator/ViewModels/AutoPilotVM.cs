04948a35c29c4f8c0914e21e9add25796eadb7e5
mirieljerbi <45950223+mirieljerbi@users.noreply.github.com>
2019-04-11 21:02:40 +0300

    cancel Button

---
+++ FlightSimulator/ViewModels/AutoPilotVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlightSimulator.ViewModels
+{
+    class AutoPilotVM :BaseNotify
+    {
+        private String stringCommandFromUser = "";
+
+        private ICommand _clickCommand;
+
+    }
+}
